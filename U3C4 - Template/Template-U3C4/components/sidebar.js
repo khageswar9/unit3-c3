bc30e415b4661c3c6a959b950fcb6387beeeda6b
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-02-21 16:35:30 +0530

    unit 3
    evalution

---
+++ U3C4 - Template/Template-U3C4/components/sidebar.js
@@ -0,0 +1,12 @@
+function sidebar() {
+
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+    return `<a href="./index.html"><h2>Khageswar News</h2></a>
+    <h4>startup</h4>
+    <h4>Audio</h4>
+    <h4>Video</h4>
+    <input type="text" id="searchbar" placeholder="search news">`
+}
+export default sidebar
